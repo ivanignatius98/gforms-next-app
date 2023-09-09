c000c4aff7356e78593a4ffdbea04d12e3df8b2d
Ivan Handoko <ivanignatius98@gmail.com>
2023-09-09 21:51:20 +0700

    separate on scroll and onclick for toolbar BUG: cannot deselect header form to index 0

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,13 @@
 import Navbar from '@modules/Navbar';
 import Footer from '@modules/Footer';
-import { ComponentPropsWithRef } from 'react';
+import React, { ComponentPropsWithRef, useState } from 'react';
+
+const MemoizedNavbar = React.memo(Navbar);
 
 const Layout: React.FC<ComponentPropsWithRef<'div'>> = ({ children }) => {
   return (
     <>
-      <Navbar />
+      <MemoizedNavbar />
       <main className="font-sans relative flex flex-col overflow-x-clip">
         <div className="flex flex-col items-center justify-center">
           <div className="w-screen max-w-[72rem]">{children}</div>
